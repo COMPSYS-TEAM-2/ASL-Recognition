19ec223b2b2df6b5c5ce3e6d2cd1c58c9c5dbf2e
DylanChambo <dylan.chambo@gmail.com>
2023-03-12 12:09:28 +1300

    refactor(neural-net): :art: refactor into classes and functions

--- src/main.py
+++ src/main.py
@@ -1,5 +1,12 @@
+from train import Train
+import matplotlib.pyplot as plt
+
+
 def main():
-    print("Hello World")
+    train = Train()
+    train.train()
+    train.test()
+    plt.show()
 
 
 if __name__ == "__main__":
--- src/train.py
+++ src/train.py
@@ -11,8 +11,66 @@ from net import Net
 
 
 class Train():
+    EPOCH = 5
+
+    def __init__(self):
+        train_df = pd.read_csv("./mnist_data/sign_mnist_train.csv")
+        test_df = pd.read_csv("./mnist_data/sign_mnist_test.csv")
+        self.trainloader = DataLoader(
+            MNIST(train_df), batch_size=4, shuffle=True)
+        self.testloader = DataLoader(
+            MNIST(test_df), batch_size=4, shuffle=True)
+        self.net = Net()
+
     def train(self):
-        pass
+        criterion = nn.CrossEntropyLoss()
+        optimizer = optim.SGD(self.net.parameters(), lr=0.001, momentum=0.9)
+
+        running_loss_list = []
+        for epoch in range(self.EPOCH):
+            running_loss = 0.0
+            for i, data in enumerate(self.trainloader, 0):
+                inputs, labels = data
+                optimizer.zero_grad()
+
+                outputs = self.net(inputs)
+                loss = criterion(outputs, labels)
+                loss.backward()
+                optimizer.step()
+
+                # print statistics
+                running_loss += loss.item()
+                if i % 200 == 199:
+                    print('[%d, %5d] loss: %.3f' %
+                          (epoch + 1, i + 1, running_loss / 200)
+                          )
+                    running_loss_list.append(running_loss)
+                    running_loss = 0.0
+        print('Finished Training')
+        plt.plot(running_loss_list)
+
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for data in self.trainloader:
+                images, labels = data
+                outputs = self.net(images)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+        print('Accuracy of the network on train images: ', correct/total)
+
+    def test(self):
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for i, data in enumerate(self.testloader, 0):
+                images, labels = data
+                outputs = self.net(images)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+        print('Accuracy of the network on test images: ', correct/total)
 
 
 class MNIST(Dataset):
@@ -30,64 +88,3 @@ class MNIST(Dataset):
         image = image.view(1, 28, 28)  # (channel, height, width)
         label = self.labels[idx]
         return (image, label)
-
-
-train_df = pd.read_csv(
-    "./mnist_data/sign_mnist_train.csv")
-test_df = pd.read_csv(
-    "./mnist_data/sign_mnist_test.csv")
-
-trainloader = DataLoader(MNIST(train_df), batch_size=4, shuffle=True)
-testloader = DataLoader(MNIST(test_df), batch_size=4, shuffle=True)
-
-net = Net()
-
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.002, momentum=0.9)
-
-running_loss_list = []
-for epoch in range(2):
-    running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
-        inputs, labels = data
-        optimizer.zero_grad()
-
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-
-        # print statistics
-        running_loss += loss.item()
-        if i % 200 == 199:
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 200)
-                  )
-            running_loss_list.append(running_loss)
-            running_loss = 0.0
-print('Finished Training')
-plt.plot(running_loss_list)
-
-correct = 0
-total = 0
-with torch.no_grad():
-    for data in trainloader:
-        images, labels = data
-        outputs = net(images)
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
-print('Accuracy of the network on train images: ', correct/total)
-
-correct = 0
-total = 0
-with torch.no_grad():
-    for i, data in enumerate(testloader, 0):
-        images, labels = data
-        outputs = net(images)
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
-print('Accuracy of the network on test images: ', correct/total)
-
-plt.show()
