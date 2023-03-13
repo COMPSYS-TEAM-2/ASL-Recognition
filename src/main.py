6a0eb657a21e2c3b1dd7c1be6c3f8b26f8701676
DylanChambo <dylan.chambo@gmail.com>
2023-03-14 11:42:43 +1300

    feat(neural-net): :sparkles: add code to load an image

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from image import prepare_image
 from network import Network
 import matplotlib.pyplot as plt
 
@@ -6,6 +7,10 @@ def main():
     network = Network()
     network.load_model()
     network.test_all()
+    image = prepare_image("data/asl/O.png")
+    print(image)
+    plt.imshow(image, cmap='gray')
+    plt.show()
 
 
 if __name__ == "__main__":
