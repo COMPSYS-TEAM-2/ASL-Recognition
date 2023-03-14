77a91708d360688585eff11ff47030c5def6accd
Dylan Chamberlain <Dylan.chambo@gmail.com>
2023-03-14 17:19:36 +1300

    Feature/add basic neural net (#3)
    * feat(neural-net): :sparkles: copy example code to build into our version
    
    * feat(neural-net): :sparkles: add basic neural net
    
    * chore: :memo: add gitignore
    
    * refactor(neural-net): :art: refactor into classes and functions
    
    * refactor: :art: minor refactoring
    
    * refactor: :art: better naming
    
    * feat(neural-net): :sparkles: introduce saving and loading nn models
    
    * docs(neural-net): :memo: add comments to each method explaining what they do
    
    * feat(neural-net): :sparkles: add code to load an image
    
    * feat(neural-net): :construction: wip
    
    * fix: :bug: Updated pandas version
    
    * feat(neural-net): :sparkles: allow to test 1 image
    
    * fix: :bug: images being inverted
    
    * refactor(neural-net): :fire: remove running loss graph
    
    ---------
    
    Co-authored-by: fosu562 <Franklino0808@gmail.com>

--- src/main.py
+++ src/main.py
@@ -1,5 +1,20 @@
+import os
+import torch
+from image import prepare_image
+from network import Network
+import matplotlib.pyplot as plt
+
+
 def main():
-    print("Hello World")
+    network = Network()
+    network.load_model()
+    network.test_all()
+    image = prepare_image("data/asl/O.png")
+    result = network.test(image)
+    _, predicted = torch.max(result, 1)
+    print(result, chr(predicted + ord('A')))
+    plt.imshow(image[0], cmap='gray')
+    plt.show()
 
 
 if __name__ == "__main__":
