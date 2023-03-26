a4c66f38a9c2b36d9db91c99b5c1a661539a2c49
DylanChambo <dylan.chambo@gmail.com>
2023-03-26 15:49:42 +1300

    feat: :art: add dark theme

--- src/main.py
+++ src/main.py
@@ -3,10 +3,13 @@ import sys
 from PyQt6.QtWidgets import QApplication
 from controller import Controller
 from gui.window import Window
+from gui.palette import set_palette
 
 
 def main():
     app = QApplication([])
+    app.setStyle("Fusion")
+    set_palette(app)
     window = Window()
     window.show()
     Controller(window)
