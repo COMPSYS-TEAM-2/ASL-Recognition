d58ad3f0241ee7725323c12ca1c3c94b662384ea
fosu562 <Franklino0808@gmail.com>
2023-04-04 16:43:31 +1200

    Able to display images

--- src/gui/trainImagesDialog.py
+++ src/gui/trainImagesDialog.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QDialog, QLabel, QGridLayout
+from PyQt6.QtWidgets import QDialog, QLabel, QGridLayout, QScrollArea
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QPixmap
 import os
@@ -10,17 +10,12 @@ class TrainImagesDialog(QDialog):
         super().__init__(parent=parent)
         self.setWindowTitle("View Training Images")
 
+        #self.scrollArea = QScrollArea(widgetResizable=True)
         self.mainLayout = QGridLayout()
-
-        label = QLabel(self)
-        pixmap = QPixmap('../../data/american_sign_language.jpg', format='jpg')
-        label.setPixmap(pixmap)
-        self.resize(pixmap.width(), pixmap.height())
-        print(pixmap.width(), pixmap.height())
-        print(pixmap.isNull())
-        print(os.path.exists("../../data/american_sign_language.jpg"))
-
-        self.mainLayout.addWidget(
-            label, 0, 0, 0, 0, Qt.AlignmentFlag.AlignCenter)
+        # self.mainLayout.addWidget(self.scrollArea)
+        self.myPixmap = QPixmap("./data/american_sign_language.PNG")
+        self.pixmapLabel = QLabel()
+        self.pixmapLabel.setPixmap(self.myPixmap)
+        self.mainLayout.addWidget(self.pixmapLabel, 0, 0)
         self.setLayout(self.mainLayout)
         self.show()
