018ddd72023951678a6b4278d69d78f24eed4c5e
fosu562 <Franklino0808@gmail.com>
2023-04-04 16:52:19 +1200

    Need to add scroll bar

--- src/gui/trainImagesDialog.py
+++ src/gui/trainImagesDialog.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QDialog, QLabel, QGridLayout, QScrollArea
+from PyQt6.QtWidgets import QDialog, QLabel, QGridLayout, QScrollArea, QScrollBar
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QPixmap
 import os
@@ -10,12 +10,15 @@ class TrainImagesDialog(QDialog):
         super().__init__(parent=parent)
         self.setWindowTitle("View Training Images")
 
-        #self.scrollArea = QScrollArea(widgetResizable=True)
+        self.scrollArea = QScrollArea(widgetResizable=True)
+        self.scrollArea.setVerticalScrollBar(Qt.ScrollBarAlwaysOn)
         self.mainLayout = QGridLayout()
-        # self.mainLayout.addWidget(self.scrollArea)
+        self.scrollLayout = QGridLayout()
+        self.scrollArea.setLayout(self.scrollLayout)
+        self.mainLayout.addWidget(self.scrollArea)
         self.myPixmap = QPixmap("./data/american_sign_language.PNG")
         self.pixmapLabel = QLabel()
         self.pixmapLabel.setPixmap(self.myPixmap)
-        self.mainLayout.addWidget(self.pixmapLabel, 0, 0)
+        self.scrollLayout.addWidget(self.pixmapLabel, 0, 0)
         self.setLayout(self.mainLayout)
         self.show()
