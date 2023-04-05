8a5d6f0ca58367adcb556bfad05ba93b5f12e1bd
bcla154 <100566232+bcla154@users.noreply.github.com>
2023-04-05 13:41:28 +1200

    going back

--- src/gui/trainDialog.py
+++ src/gui/trainDialog.py
@@ -2,6 +2,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 from PyQt6.QtWidgets import QPushButton, QComboBox, QDialog, QProgressBar, QTextBrowser,QMessageBox,QVBoxLayout
+<<<<<<< HEAD
 from PyQt5.QtWidgets import *
 =======
 =======
@@ -9,18 +10,11 @@ from PyQt5.QtWidgets import *
 from PyQt6.QtWidgets import QPushButton, QComboBox, QDialog, QProgressBar, QTextBrowser
 from gui.trainingPrgBar import *
 >>>>>>> parent of 570a0fe (Popup working)
+=======
+from gui.trainingPrgBar import *
+>>>>>>> parent of f179658 (Bugged even worse)
 
-class ProgBarWindow(QWidget):
 
-    def __init__(self):
-        self.layout = QVBoxLayout()
-        self.label = QLabel("Training Progress")
-        self.layout.addWidget(self.label)
-        self.pbar = QProgressBar(self)
-        self.pbar.resize(290, 30)
-        self.pbar.setMaximum(100)
-        self.layout.addWidget(self.pbar)
-        self.show()
 
 class TrainDialog(QDialog):
     # Dialog to train the database
@@ -34,7 +28,10 @@ class TrainDialog(QDialog):
         self.textBrowserTrain.resize(290, 320)
         self.textBrowserTrain.move(5, 5)
 
-        
+        # Progress Bar
+        self.pbar = QProgressBar(self)
+        self.pbar.resize(290, 30)
+        self.pbar.move(5, 335)
 
         # Combo button
         self.comboBtn = QComboBox(self)
@@ -44,12 +41,12 @@ class TrainDialog(QDialog):
         self.comboBtn.addItems(itemsList)
         self.comboBtn.show()
 
-
         # Train Button
         self.train_btn = QPushButton('Train', self)
         self.train_btn.resize(95, 20)
         self.train_btn.move(100, 375)
         # self.train_btn.clicked.connect(self.train_dataset)  # Train the dataset
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
         self.train_btn.clicked.connect(ProgBarWindow)
@@ -59,6 +56,9 @@ class TrainDialog(QDialog):
 =======
         self.train_btn.clicked.connect(self.trainProgBar())
 >>>>>>> parent of 570a0fe (Popup working)
+=======
+        self.train_btn.clicked.connect(self.show_popUp)
+>>>>>>> parent of f179658 (Bugged even worse)
 
 
         # Cancel Button
@@ -66,6 +66,7 @@ class TrainDialog(QDialog):
         self.cancel_btn.resize(95, 20)
         self.cancel_btn.move(200, 375)
         self.cancel_btn.clicked.connect(self.close)
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
     
@@ -86,5 +87,33 @@ class TrainDialog(QDialog):
     def trainProgBar(self):
         prgb = TrainingPrgBar(self) 
 >>>>>>> parent of 570a0fe (Popup working)
+=======
+
+   
+    
+    def showPbar(self):
+
+        self.setWindowTitle('Progress Bar')
+        self.progress = QProgressBar(self)
+        self.progress.setGeometry(0, 0, 300, 25)
+        self.progress.setMaximum(100)
+        self.button = QPushButton('Start', self)
+        self.button.move(0, 30)
+
+        self.show()
+
+        self.button.clicked.connect(self.onButtonClick)
+
+    def onButtonClick(self):
+        count = 0
+        while count < 200:
+            count += 1
+            time.sleep(1)
+            self.progress.setValue(count)
+
+    def show_popUp(self):
+        self.showPbar()
+
+>>>>>>> parent of f179658 (Bugged even worse)
 
 
