f179658646cfe30ba64353b6a02ad01a9e2aeeea
bcla154 <100566232+bcla154@users.noreply.github.com>
2023-04-05 13:38:04 +1200

    Bugged even worse

--- src/gui/trainDialog.py
+++ src/gui/trainDialog.py
@@ -1,8 +1,18 @@
 # Therefore the initial window should have buttons to import data or view the dataset
 from PyQt6.QtWidgets import QPushButton, QComboBox, QDialog, QProgressBar, QTextBrowser,QMessageBox,QVBoxLayout
-from gui.trainingPrgBar import *
+from PyQt5.QtWidgets import *
 
+class ProgBarWindow(QWidget):
 
+    def __init__(self):
+        self.layout = QVBoxLayout()
+        self.label = QLabel("Training Progress")
+        self.layout.addWidget(self.label)
+        self.pbar = QProgressBar(self)
+        self.pbar.resize(290, 30)
+        self.pbar.setMaximum(100)
+        self.layout.addWidget(self.pbar)
+        self.show()
 
 class TrainDialog(QDialog):
     # Dialog to train the database
@@ -16,10 +26,7 @@ class TrainDialog(QDialog):
         self.textBrowserTrain.resize(290, 320)
         self.textBrowserTrain.move(5, 5)
 
-        # Progress Bar
-        self.pbar = QProgressBar(self)
-        self.pbar.resize(290, 30)
-        self.pbar.move(5, 335)
+        
 
         # Combo button
         self.comboBtn = QComboBox(self)
@@ -29,12 +36,13 @@ class TrainDialog(QDialog):
         self.comboBtn.addItems(itemsList)
         self.comboBtn.show()
 
+
         # Train Button
         self.train_btn = QPushButton('Train', self)
         self.train_btn.resize(95, 20)
         self.train_btn.move(100, 375)
         # self.train_btn.clicked.connect(self.train_dataset)  # Train the dataset
-        self.train_btn.clicked.connect(self.show_popUp)
+        self.train_btn.clicked.connect(ProgBarWindow)
 
 
         # Cancel Button
@@ -42,31 +50,13 @@ class TrainDialog(QDialog):
         self.cancel_btn.resize(95, 20)
         self.cancel_btn.move(200, 375)
         self.cancel_btn.clicked.connect(self.close)
-
-   
     
-    def showPbar(self):
+    
+        
+        
+    
+        
 
-        self.setWindowTitle('Progress Bar')
-        self.progress = QProgressBar(self)
-        self.progress.setGeometry(0, 0, 300, 25)
-        self.progress.setMaximum(100)
-        self.button = QPushButton('Start', self)
-        self.button.move(0, 30)
-
-        self.show()
-
-        self.button.clicked.connect(self.onButtonClick)
-
-    def onButtonClick(self):
-        count = 0
-        while count < 200:
-            count += 1
-            time.sleep(1)
-            self.progress.setValue(count)
-
-    def show_popUp(self):
-        self.showPbar()
-
+       
 
 
