ea6439b6f7f43bee62d1994868238898f7cd9551
fosu562 <Franklino0808@gmail.com>
2023-04-14 17:24:00 +1200

    Images are able to be filtered

--- src/gui/testImagesDialog.py
+++ src/gui/testImagesDialog.py
@@ -1,9 +1,8 @@
-from PyQt6.QtWidgets import QDialog, QLabel, QGridLayout, QScrollArea, QFormLayout, QGroupBox
+from PyQt6.QtWidgets import QDialog, QLabel, QGridLayout, QScrollArea, QFormLayout, QGroupBox, QLineEdit
 from PyQt6.QtGui import QPixmap
 from PyQt6.QtCore import QTimer, QCoreApplication
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import PIL.Image as pil
 from PIL.ImageQt import ImageQt
 
@@ -13,21 +12,27 @@ class TestImagesDialog(QDialog):
         super().__init__(parent=parent)
         self.setWindowTitle("View Testing Images")
         self.setGeometry(0, 0, 350, 500)
+        self.mainLayout = QGridLayout()
         self.scrollArea = QScrollArea(widgetResizable=True)
         self.scrollLayout = QFormLayout()
+        self.inputBoxLabel = QLabel("Filter")
+        self.lineEdit = QLineEdit()
+        self.lineEdit.textChanged.connect(self.lineEditChanged)
+        self.currentFilter = ""
+        self.mainLayout.addWidget(self.inputBoxLabel)
+        self.mainLayout.addWidget(self.lineEdit)
+
         groupBox = QGroupBox()
         groupBox.setLayout(self.scrollLayout)
 
         self.scrollArea.setWidget(groupBox)
-        self.mainLayout = QGridLayout()
         self.mainLayout.addWidget(self.scrollArea)
         self.setLayout(self.mainLayout)
         self.show()
 
-        self.closeEvent = self.dialogClosed
+        self.closeEvent = self.exitWindow
 
         self._iter = iter(range(10000))
-        # Splitting image set into intervals of 1000
         self._timer = QTimer(interval=30, timeout=self.dynamic_loading)
         self._timer.start()
 
@@ -44,20 +49,35 @@ class TestImagesDialog(QDialog):
             h = 28
 
             for i in range(i):
-                QCoreApplication.processEvents()
-                sample = np.reshape(
-                    data[data.columns[1:]].iloc[i].values/255, (w, h))
-                img = pil.fromarray(np.uint8(sample*255), 'L')
-                # qImg = QImage(sample.data, w, h, w,
-                #               QImage.Format.Format_Grayscale8)
-                qimg = ImageQt(img)
-                # qPixMap = QPixmap.fromImage(qImg.scaled(10*w, 10*h))
-                qPixMap = QPixmap.fromImage(qimg).scaled(2*w, 2*h)
-                qPixMapLabel = QLabel()
-                qPixMapLabel.setPixmap(qPixMap)
-                self.scrollLayout.addRow(qPixMapLabel)
-                nameLabel = QLabel(alphabet[int(data["label"].iloc[i])])
-                self.scrollLayout.addRow(nameLabel)
+                if alphabet[int(data["label"].iloc[i])] == self.currentFilter or self.currentFilter == "":
+                    QCoreApplication.processEvents()
+                    sample = np.reshape(
+                        data[data.columns[1:]].iloc[i].values/255, (w, h))
+                    img = pil.fromarray(np.uint8(sample*255), 'L')
+                    qimg = ImageQt(img)
+                    qPixMap = QPixmap.fromImage(qimg).scaled(2*w, 2*h)
+                    qPixMapLabel = QLabel()
+                    qPixMapLabel.setPixmap(qPixMap)
+                    self.scrollLayout.addRow(qPixMapLabel)
+                    nameLabel = QLabel(alphabet[int(data["label"].iloc[i])])
+                    self.scrollLayout.addRow(nameLabel)
 
-    def dialogClosed(self, event):
+    def exitWindow(self, event):
+        self.stopTimer()
+
+    def stopTimer(self):
         self._timer.stop()
+
+    def lineEditChanged(self, text):
+        self.stopTimer()
+        # When the line edit is changed I need to take the value and refresh the whole window.
+        self.currentFilter = text
+        self.clearWindow()
+        self._iter = iter(range(10000))
+        self._timer = QTimer(interval=30, timeout=self.dynamic_loading)
+        self._timer.start()
+
+    def clearWindow(self):
+        # Clear the window
+        for i in reversed(range(self.scrollLayout.count())):
+            self.scrollLayout.itemAt(i).widget().setParent(None)
