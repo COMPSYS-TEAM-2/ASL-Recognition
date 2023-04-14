cdd84ff301389cf583dc464352f92c893e06bf17
fosu562 <Franklino0808@gmail.com>
2023-04-14 14:23:00 +1200

    feat: :sparkles: Images display in a loading manner, though this freezes the window for seconds at a time

--- src/gui/trainImagesDialog.py
+++ src/gui/trainImagesDialog.py
@@ -1,5 +1,11 @@
 from PyQt6.QtWidgets import QDialog, QLabel, QGridLayout, QScrollArea, QFormLayout, QGroupBox
 from PyQt6.QtGui import QPixmap
+from PyQt6.QtCore import QTimer
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import PIL.Image as pil
+from PIL.ImageQt import ImageQt
 
 
 class TrainImagesDialog(QDialog):
@@ -7,28 +13,46 @@ class TrainImagesDialog(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent=parent)
         self.setWindowTitle("View Training Images")
-        self.setGeometry(0, 0, 950, 500)
+        self.setGeometry(0, 0, 350, 500)
         self.scrollArea = QScrollArea(widgetResizable=True)
         self.scrollLayout = QFormLayout()
-
         groupBox = QGroupBox()
         groupBox.setLayout(self.scrollLayout)
-
-        self.myPixmap = QPixmap("./data/american_sign_language.PNG")
-        self.pixmapLabel = QLabel()
-        self.pixmapLabel.setPixmap(self.myPixmap)
-        self.scrollLayout.addRow(self.pixmapLabel)
-        self.labelName = QLabel("MINST Training set")
-        self.scrollLayout.addRow(self.labelName)
-        self.myPixmap = QPixmap("./data/american_sign_language.PNG")
-        self.pixmapLabel = QLabel()
-        self.pixmapLabel.setPixmap(self.myPixmap)
-        self.scrollLayout.addRow(self.pixmapLabel)
-        self.labelName = QLabel("MINST Training set")
-        self.scrollLayout.addRow(self.labelName)
 
         self.scrollArea.setWidget(groupBox)
         self.mainLayout = QGridLayout()
         self.mainLayout.addWidget(self.scrollArea)
         self.setLayout(self.mainLayout)
         self.show()
+
+        self._iter = iter(range(60000))
+        # Splitting image set into intervals of 1000
+        self._timer = QTimer(interval=3000, timeout=self.dynamic_loading)
+        self._timer.start()
+
+    def dynamic_loading(self):
+        try:
+            i = next(self._iter)
+        except StopIteration:
+            self._timer.stop()
+        else:
+            alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+                        "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+            data = pd.read_csv('./data/sign_mnist_train/sign_mnist_train.csv')
+            w = 28
+            h = 28
+
+            for i in range(0, 26):
+                sample = np.reshape(
+                    data[data.columns[1:]].iloc[i].values/255, (w, h))
+                img = pil.fromarray(np.uint8(sample*255), 'L')
+                # qImg = QImage(sample.data, w, h, w,
+                #               QImage.Format.Format_Grayscale8)
+                qimg = ImageQt(img)
+                # qPixMap = QPixmap.fromImage(qImg.scaled(10*w, 10*h))
+                qPixMap = QPixmap.fromImage(qimg).scaled(5*w, 5*h)
+                qPixMapLabel = QLabel()
+                qPixMapLabel.setPixmap(qPixMap)
+                self.scrollLayout.addRow(qPixMapLabel)
+                nameLabel = QLabel(alphabet[int(data["label"].iloc[i])])
+                self.scrollLayout.addRow(nameLabel)
